2cbf800ecdcd6c117fb1e49270db946d038c6aef
Prem Kumar <prem68265@gmail.com>
2024-12-07 08:32:59 +0530

    db

--- db.js
+++ db.js
@@ -1,8 +1,23 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect('mongodb://127.0.0.1/Watrken');
+//     console.log('Database connected');
+//   } catch (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1/Watrken');
+    await mongoose.connect('mongodb+srv://Prem:Prem7366@cluster0.x2ajb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('Database connected');
   } catch (err) {
     console.error(err);
@@ -11,5 +26,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
 
